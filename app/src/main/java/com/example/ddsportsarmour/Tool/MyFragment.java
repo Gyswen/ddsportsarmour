2bda4533a9f665b98c9791fc0f3d640babf9309e
郭义帅 <guoyishuai@zhgt999.com>
2020-11-06 18:11:37 +0800

    上传代码

---
+++ app/src/main/java/com/example/ddsportsarmour/Tool/MyFragment.java
@@ -0,0 +1,21 @@
+package com.example.ddsportsarmour.Tool;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public abstract class MyFragment extends Fragment {
+    protected abstract int ContentView();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(ContentView(),container,false);
+        return view;
+    }
+}
